e4dcee24a71aabf20eeae877ef55c79c351e1d3c
Kavita Vhanmane <kavitavhanmane2@gmail.com>
2023-08-20 14:21:32 +0100

    SecondView added

---
+++ TipCal/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  TipCal
+//
+//  Created by pk on 20/08/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Hello SecondView")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
