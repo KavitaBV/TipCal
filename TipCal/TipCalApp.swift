bcdc36f456487f8bcb344e7b482d968d9c67cb17
Kavita Vhanmane <kavitavhanmane2@gmail.com>
2023-08-20 13:55:29 +0100

    Initial Commit

---
+++ TipCal/TipCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalApp.swift
+//  TipCal
+//
+//  Created by pk on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TipCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
